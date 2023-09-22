106c673c90a14e08d7ae6beac1d4c2561bbe3fc7
Gabriela B. Augsten <gabriela.augsten@duque.g12.br>
2023-09-21 21:14:03 -0300

    Criação da tela da calculadora

---
+++ src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author 08110
+ */
+public class Calculadora {
+    public static void main(String[] args) {
+        FrmCalculadora calc = new FrmCalculadora();
+        calc.setLocationRelativeTo(calc);
+        calc.setVisible(true);
+    }
+    
+}
